eff63a5779589e31b78f942b5c50b8491135519f
Arun Pratap <arun@Aruns-MacBook-Air.local>
2019-11-13 14:38:49 +0530

    changes

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
 
 class FrontController extends Controller
 {
     public function index()
     {
+        if (Auth::user()->is_admin) {
+            return redirect()->route('admin_dashboard');
+        } else if (Auth::user()->is_admin == 0) {
+            return redirect()->route('user_groups');
+        }
         return view('home');
     }
 }
--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -30,11 +30,11 @@
     <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
       <!-- Sidebar - Brand -->
-      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/">
-        <div class="sidebar-brand-icon rotate-n-15">
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{route('home')}}">
+        {{-- <div class="sidebar-brand-icon rotate-n-15">
           <i class="fas fa-laugh-wink"></i>
-        </div>
-        <div class="sidebar-brand-text mx-3">{{ config('app.name', 'Laravel') }} </div>
+        </div> --}}
+        <div class="sidebar-brand-text"><img src="/images/header-the-network-film-productions-october16.png" alt="" width="150px"></div>
       </a>
 
       <!-- Divider -->
